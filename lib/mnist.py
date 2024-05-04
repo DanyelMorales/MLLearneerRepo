fa21b9e0c7dc394a174794194f4a8baa094c1900
Daniel V Morales <danyelmorales1991@gmail.com>
2024-05-03 23:41:44 -0700

    Update mnist.py

--- lib/mnist.py
+++ lib/mnist.py
@@ -51,8 +51,8 @@ def one_hot_encode(Y, exact_match=False, n_classes=10):
 def extract_test_data(data_np, test_size=0.23):
   test_size = round(data_np.shape[0] * test_size)
   print(f"test_size: {test_size}")
-  X_train = data_np[:test_size]
-  X_test = data_np[test_size:]
+  X_test = data_np[:test_size]
+  X_train = data_np[test_size:]
 
   Y_train = X_train[:,-1]
   Y_test = X_test[:,-1]

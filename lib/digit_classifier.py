90140204c9e98f41f71c0f4a89c2d67bafcffbf7
Daniel V Morales <danyelmorales1991@gmail.com>
2024-05-04 02:54:42 -0700

    Update digit_classifier.py

--- lib/digit_classifier.py
+++ lib/digit_classifier.py
@@ -48,7 +48,7 @@ def report(iteration, X_train, Y_train, X_test, Y_test, w, index_to_value=None):
     classified=np.array(classified_main)
 
   matches = np.count_nonzero(classified == Y_test)
-  print(f"{matches},Y_test,classified")
+  print(f"{classified}\n\n\n{Y_test}")
   n_test_examples = Y_test.shape[0]
   matches = matches * 100.0 / n_test_examples
   training_loss = loss(X_train, Y_train, w)

8d0590125827bb35299bc3279fbbad870ed40410
Daniel V Morales <danyelmorales1991@gmail.com>
2024-05-03 23:49:01 -0700

    Update mnist.py

--- lib/mnist.py
+++ lib/mnist.py
@@ -53,7 +53,7 @@ def extract_test_data(data_np, test_size=0.23):
   print(f"test_size: {test_size}")
   X_test = data_np[:test_size]
   X_train = data_np[test_size:]
-
+  print(X_test)
   Y_train = X_train[:,-1]
   Y_test = X_test[:,-1]
   X_train = X_train[:,:-1]
